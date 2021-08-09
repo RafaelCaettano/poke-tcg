db4d829db8fab4fd82cf940fcae32aa067be653b
RafaelCaettano <rafac.docarmo@hotmail.com>
2021-08-09 12:13:24 -0300

    feat: criando o service error

---
+++ src/app/shared/models/error.interface.ts
@@ -0,0 +1,6 @@
+export interface Error {
+    icon: string;
+    title: string;
+    description: string;
+    link: string;
+}
---
+++ src/app/shared/services/error/error.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { Error } from '../../models/error.interface';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class ErrorService {
+
+	error = new BehaviorSubject<Error>({
+		icon: '',
+		title: '',
+		description: '',
+		link: ''
+	});
+
+	errors = new Map([
+    [
+      'GENERIC',
+      {
+        icon: '',
+        title: '',
+        description: '',
+        link: ''
+      }
+    ]
+	]);
+
+	constructor(private router: Router) { }
+
+	setError(error: string = 'GENERIC'): void {
+		this.errors.forEach((value: Error, key: string) => {
+			if(key === error) {
+				this.error.next(value);
+			}
+		});
+	}
+
+}
